4fb9573e81335d889689fbe16ccb7920af5e6289
Shubham Kumar <2090944@cognizant.com>
2024-08-09 20:10:14 +0530

    Day_2 || Create Request and Response DTO For Web

--- Mango.Web/Models/ResponceDTOs.cs
+++ Mango.Web/Models/ResponceDTOs.cs
@@ -2,9 +2,9 @@
 {
     public class ResponceDTOs
     {
-
         public object? Result { get; set; }
         public bool IsSuccess { get; set; } = true;
         public string Message { get; set; } = "";
     }
+
 }
---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            Get, Post, Put, Delete
+        }
+    }
+}
