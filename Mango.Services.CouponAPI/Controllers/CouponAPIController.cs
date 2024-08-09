928d34c7f4c2a4bcd8a67f81d7e7748e2dd0cdc5
Shubham Kumar <2090944@cognizant.com>
2024-08-09 17:25:49 +0530

    Day_2 || Add AutoMaper
    But Not Working

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Mango.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Azure;
+using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.DTOs;
 using Microsoft.AspNetCore.Http;
@@ -14,12 +16,14 @@ namespace Mango.Services.CouponAPI.Controllers
         #region CTOR
 
         private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
         private ResponceDTOs _responce;
 
-        public CouponAPIController(AppDbContext db)
+        public CouponAPIController(AppDbContext db,IMapper mapper)
         {
             _db = db;
-            _responce=new ResponceDTOs();
+          _mapper = mapper;
+            _responce =new ResponceDTOs();
         }
 
 
@@ -33,7 +37,9 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                //_responce.Result =_mapper.Map<IEnumerable<CouponDTOs>>(objList);
                 _responce.Result = objList;
+
             }
             catch (Exception ex)
             {
@@ -53,6 +59,8 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 Coupon obj = _db.Coupons.First(a => a.CouponId == id);
+                //_responce.Result =_mapper.Map<CouponDTOs>(obj);
+
                 _responce.Result = obj;
             }
             catch (Exception ex)
