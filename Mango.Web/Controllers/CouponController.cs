dc42710c9ece7c3eb0259026c95154717e618726
Shubham Kumar <2090944@cognizant.com>
2024-08-13 20:31:04 +0530

    Day_5 || Coupon Controller and API call iin Action

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDTOs>? list = [];
+
+            ResponceDTOs? responce = await _couponService.GetAllCouponsAsync();
+            if (responce != null && responce.IsSuccess)
+            {
+
+                list = JsonConvert.DeserializeObject<List<CouponDTOs>>(Convert.ToString(responce.Result));
+
+            }
+            return View(list);
+        }
+    }
+}
