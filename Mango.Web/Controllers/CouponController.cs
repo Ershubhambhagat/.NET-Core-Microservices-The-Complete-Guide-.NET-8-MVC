36fcb2bfc04729c630f518e07856bfbd670800cd
Shubham Kumar <2090944@cognizant.com>
2024-08-14 17:51:53 +0530

    Day_6 || Create Coupon functionality Working Fine

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,14 +2,12 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
 namespace Mango.Web.Controllers
 {
     public class CouponController : Controller
     {
         #region CTOR
         private readonly ICouponService _couponService;
-
         public CouponController(ICouponService couponService)
         {
             _couponService = couponService;
@@ -20,22 +18,38 @@ namespace Mango.Web.Controllers
         public async Task<IActionResult> CouponIndex()
         {
             List<CouponDTOs>? list = new();
-
             ResponceDTOs? responce = await _couponService.GetAllCouponsAsync();
             if (responce != null && responce.IsSuccess)
             {
-
                 list = JsonConvert.DeserializeObject<List<CouponDTOs>>(Convert.ToString(responce.Result));
-
             }
             return View(list);
-        } 
+        }
         #endregion
 
+        #region Create Coupon
+
+        #region This is only for page Create Coupon Page Open
         public async Task<IActionResult> CouponCreate()
         {
             return View();
         }
+        #endregion
 
-	}
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDTOs model)
+        {
+            //Server side validation 
+            if (ModelState.IsValid) 
+            {
+                ResponceDTOs responce = await _couponService.CreateCouponsAsync(model);
+                if (responce != null && responce.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+        #endregion
+    }
 }
