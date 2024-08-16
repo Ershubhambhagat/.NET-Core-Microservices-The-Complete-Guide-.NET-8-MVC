b713749381ea8fa3748d06855b3093bd5a049121
Shubham Kumar <2090944@cognizant.com>
2024-08-16 21:02:07 +0530

    Day_7 || Auth:  Endpoints for Login and Register

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        private readonly DbContext _dbContext;
+
+        public AuthAPIController(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #region register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+        #endregion
+        #region Login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        } 
+        #endregion
+    }
+}
