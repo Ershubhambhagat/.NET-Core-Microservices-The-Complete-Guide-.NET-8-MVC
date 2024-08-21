ff5e818e5c25c7b1b5a3e94c07fec694ff8234c3
Shubham Kumar <2090944@cognizant.com>
2024-08-21 20:00:31 +0530

    Day_9 || AuthWeb : . Auth Controller in Web Project

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        #region CTOR
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        } 
+        #endregion
+
+        #region Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO login = new();
+            return View(login);
+        }
+        #endregion
+
+        #region Register
+        [HttpGet]
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+
+        #endregion
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
