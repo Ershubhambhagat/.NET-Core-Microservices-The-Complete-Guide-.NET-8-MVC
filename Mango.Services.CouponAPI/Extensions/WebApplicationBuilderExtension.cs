6dc507c03805c39bea4c74835c4e5f7114c2ec06
Shubham Kumar <2090944@cognizant.com>
2024-09-16 21:18:29 +0530

    Day_11 || Code Clean Up

---
+++ Mango.Services.CouponAPI/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Services.CouponAPI.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.Services.CouponAPI.Extensions
+{
+    public static class WebApplicationBuilderExtension
+    {
+        public static WebApplicationBuilder addApplicationBuilder(this WebApplicationBuilder builder)
+        {
+
+            //Getting All ApiSetting from AppSetting.json
+            var settingSection = builder.Configuration.GetSection("ApiSetting");
+
+            var secret = settingSection.GetValue<string>("Secret");
+            var Issuer = settingSection.GetValue<string>("Issuer");
+            var Audience = settingSection.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = Issuer,
+                    ValidAudience = Audience,
+                    ValidateAudience = true,
+
+                };
+            });
+            return builder;
+        }
+
+    }
+}
